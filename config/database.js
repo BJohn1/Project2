287ea45932ddfdb229a063798e9a1e23cbf40d2e
Blake <b.adeyemijohn@gmail.com>
2020-01-23 14:33:41 -0800

    update database config file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/alltime5',{
+mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true //get ride of deprication warning
